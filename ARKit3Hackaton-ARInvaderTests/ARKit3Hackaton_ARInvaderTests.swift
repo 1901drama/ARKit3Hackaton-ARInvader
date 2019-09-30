1aced4a03e6edd827d5f7a0e5275705b272119c6
drama <1901drama@gmail.com>
2019-10-01 01:12:17 +0900

    Initial Commit

---
+++ ARKit3Hackaton-ARInvaderTests/ARKit3Hackaton_ARInvaderTests.swift
@@ -0,0 +1,34 @@
+//
+//  ARKit3Hackaton_ARInvaderTests.swift
+//  ARKit3Hackaton-ARInvaderTests
+//
+//  Created by タダ・ドラマ on 2019/10/01.
+//  Copyright © 2019 1901drama. All rights reserved.
+//
+
+import XCTest
+@testable import ARKit3Hackaton_ARInvader
+
+class ARKit3Hackaton_ARInvaderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
